66eb7134b8a44cf621f81228faed06a345d43021
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-07-22 21:53:12 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,13 +15,13 @@ export function App() {
   const onLeaveFeedback = option => {
     switch (option) {
       case 'good':
-        setGood(good + 1);
+        setGood(prevState => prevState + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prevState => prevState + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prevState => prevState + 1);
         break;
       default:
         return;
